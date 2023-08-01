96d8d2b5b432163b2646e6852f84dda27703421c
Gabriele <cristiine94@outlook.com>
2023-08-01 10:16:56 -0300

    Criando model e services - @GabrieleeCris

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,25 @@
-import './App.css';
-import Home from './pages/home/Home';
-import Navbar from './components/navBar/NavBar';
-import Footer from './components/footer/Footer';
+import React from 'react';
+import UserProvider from './contexts/UserContext';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/home/Home';
 import Login from './pages/login/Login';
-import Sobre from './pages/sobre/Sobre';
+
 
 function App() {
+
   return (
-    <>
-    <BrowserRouter>
-        <Navbar />
-          <div className='min-h-[80vh]'>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/home" element={<Home />} />
-              <Route path="/sobre" element={<Sobre />} />
-            </Routes>
-          </div>
-          <Footer />
-        </BrowserRouter>
-    </>
-);
+    <UserProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+
+      </BrowserRouter>
+
+    </UserProvider>
+  );
 }
+
 export default App;
