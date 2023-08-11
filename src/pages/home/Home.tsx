6b15a5ec5696e40329b4a02801ac19eec7f9f506
JayCesar <batistajulio215@gmail.com>
2023-08-11 14:32:53 -0300

    @JayCesar | update: adiciona o ícone da lua / react

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -4,8 +4,8 @@ import colheita from "../../assets/colheita.svg";
 // import Sobre from "../../pages/sobre/Sobre";
 import heroImage from "../../assets/hero.svg";
 import heroImage2 from "../../assets/hero2.svg";
-// import { AiFillGithub } from 'react-icons/Ai'
-// import { AiFillLinkedin } from 'react-icons/Ai'
+import { AiFillGithub } from 'react-icons/ai'
+import { AiFillLinkedin } from 'react-icons/ai'
 import { Link } from 'react-router-dom'
 import { useEffect } from 'react';
 
