c14905ebf2139b5f9a79baedcda5b83dfd3602e2
Larissasbuenno <larissasbuenno@gmail.com>
2023-08-12 16:37:34 -0300

    Info: atualização page Perfil

--- src/pages/perfil/Perfil.tsx
+++ src/pages/perfil/Perfil.tsx
@@ -1,8 +1,9 @@
 import { useContext, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { AuthContext } from '../../contexts/AuthContext'
-import loginLogo from '../../assets/nutri.png'
+// import loginLogo from '../../assets/nutri.png'
 import { toastAlerta } from '../../utils/toastAlerta'
+
 function Perfil() {
   let navigate = useNavigate()
 
@@ -16,15 +17,21 @@ function Perfil() {
   }, [usuario.token])
 
   return (
-    <div className='container mx-auto mt-4 rounded-2xl overflow-hidden'>
-      <img className='w-full h-72 object-cover border-b-8 border-white' src={loginLogo} alt="Capa do Perfil" />
-      <img src={usuario.foto} alt={`Foto de perfil de ${usuario.nome}`} className='rounded-full w-56 mx-auto mt-[-8rem] border-8 border-white relative z-10' />
-      <div className="relative mt-[-6rem] h-72 flex flex-col bg-sky-500 text-white text-2xl items-center justify-center">
+
+    <div className="container mx-auto mt-4 rounded-2xl overflow-hidden">
+
+      <div className='w-full h-72 object-cover border-b-8 border-white ' />
+
+      <img src={usuario.foto} alt={`Foto de perfil de ${usuario.nome}`} className='rounded-full w-56 mx-auto mt-[-8rem] border-8 border-white relative z-10 ' />
+
+      <div className="relative mt-[-6rem] h-72 flex flex-col bg-verde_neutro text-white text-2xl items-center justify-center ">
         <p>Nome: {usuario.nome} </p>
         <p>Email: {usuario.usuario}</p>
       </div>
+
     </div>
   )
 }
 
 export default Perfil
+
